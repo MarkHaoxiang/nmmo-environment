8a0ce91f30da7074e1f68a459a3c7c1214ac3238
Houwang123 <mark.haoxiang@gmail.com>
2023-01-11 13:41:09 +0000

    Continued implementation of a subset of variable tasks

--- nmmo/lib/task/variable.py
+++ nmmo/lib/task/variable.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Dict, List
+from math import prod
 
 class Variable:
     '''
@@ -74,29 +75,39 @@ class Sum(Variable):
         return ["+"] + [op.description() for op in self._operands]
 
 class Product(Variable):
-    pass
-
-class Max(Variable):
-    pass
-
-class Min(Variable):
-    pass
-
-###############################################################
-
-class InflictDamage(TargetVariable):
-    def __init__(self, target: VariableTarget, damage_type: int):
-        super().__init__(target)
-        self._damage_type = damage_type
-
-    def value(self, realm, entity) -> int:
-        # TODO(daveey) damage_type is ignored, needs to be added to entity.history
-        return sum([
-            realm.entity(a).history.damage_inflicted for a in self._target.agents()
-        ])
+    def __init__(self, *operands):
+        super().__init__()
+        self._operands = operands
+    
+    def value(self, realm, entity):
+        return prod([op.value(realm, entity) for op in self._operands])
 
     def description(self) -> List:
-        return super().description() + [self._damage_type]
+        return ["x"] + [op.description() for op in self._operands]
+
+class Max(Variable):
+    def __init__(self, *operands):
+        super().__init__()
+        self._operands = operands
+    
+    def value(self, realm, entity):
+        return max([op.value(realm, entity) for op in self._operands])
+
+    def description(self) -> List:
+        return ["max"] + [op.description() for op in self._operands]
+
+class Min(Variable):
+    def __init__(self, *operands):
+        super().__init__()
+        self._operands = operands
+    
+    def value(self, realm, entity):
+        return min([op.value(realm, entity) for op in self._operands])
+
+    def description(self) -> List:
+        return ["min"] + [op.description() for op in self._operands]
+
+###############################################################
 
 class Alive(TargetVariable):
     def __init__(self, target: VariableTarget):
@@ -113,13 +124,96 @@ class Health(TargetVariable):
         return sum([realm.entity(a).resources.health.val for a in self._target.agents()])
         
 class Gold(TargetVariable):
-    pass
+    def __init__(self,target: VariableTarget):
+        super().__init__(target)
+    
+    def value(self, realm, entity):
+        return sum([realm.entity(a).inventory.gold.quantity.val for a in self._target.agents()])
+        
 
 class Group(TargetVariable):
-    pass
+    '''
+    Maximum group size within a bounding box of (distance x distance)
+    '''
+    def __init__(self,target: VariableTarget, distance : int):
+        super().__init__(target)
+        self.distance = distance
 
-class Level(TargetVariable):
-    pass
+    def value(self, realm, entity):
+        positions = [realm.entity(a).pos for a in self._target.agents()]
+        positions.sort() # Sort by r
+        res = 1
+        for i,a in enumerate(positions):
+            temp = 1
+            for j in range(i,len(positions)):
+                b = positions[j]
+                if a[0] + self.distance <= b[0]:
+                    break
+                if a[1] + self.distance > b[0]:
+                    temp += 1
+            res = max(res,temp)
+        return res
+
+from nmmo.systems.skill import Mage, Range, Melee, Fishing, Herbalism, Prospecting, Carving, Alchemy
+class SkillLevel(TargetVariable, ABC):
+    def __init__(self, target: VariableTarget, skill):
+        super().__init__(target)
+        self.skill = skill
+    
+    def value(self, realm, entity):
+        class2func = {
+            Mage: lambda player: player.skills.mage.level.val,
+            Range: lambda player: player.skills.mage.level.val,
+            Melee: lambda player: player.skills.mage.level.val,
+            Fishing: lambda player: player.skills.mage.level.val,
+            Herbalism: lambda player: player.skills.mage.level.val,
+            Prospecting: lambda player: player.skills.mage.level.val,
+            Carving: lambda player: player.skills.mage.level.val,
+            Alchemy: lambda player: player.skills.mage.level.val,
+        }
+
+        return max([class2func[self.skill](realm.entity(a)) for a in self._target.agents()])
+
+
+# TODO: Implementation of below incomplete
 
 class Harvest(TargetVariable):
-    pass
+    '''
+    target: The team that is completing the task. Any agent may complete
+    resource: lib.material to harvest
+    level: minimum material level to harvest
+    '''
+    def __init__(self, target: VariableTarget, resource, level: int):
+        super().__init__(target)
+        self.resource = resource
+        self.level = level
+
+    def value(Self, realm, entity):
+        # TODO(mark) need to add into entity.history 
+        raise NotImplementedError
+
+class InflictDamage(TargetVariable):
+    def __init__(self, target: VariableTarget, damage_type: int):
+        super().__init__(target)
+        self._damage_type = damage_type
+
+    def value(self, realm, entity) -> int:
+        # TODO(daveey) damage_type is ignored, needs to be added to entity.history
+        return sum([
+            realm.entity(a).history.damage_inflicted for a in self._target.agents()
+        ])
+
+    def description(self) -> List:
+        return super().description() + [self._damage_type]
+
+class EquipLevel(TargetVariable):
+    '''
+    Returns maximum level of 'equipment' equipped on target, default 0
+    '''
+
+    def __init__(self, target: VariableTarget, equipment):
+        super().__init__(target)
+        self.equipment = equipment
+
+    def value(self, realm, entity):
+        raise NotImplementedError
